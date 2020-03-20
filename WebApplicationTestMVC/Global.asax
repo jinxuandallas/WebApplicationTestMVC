5848d2c0fb1a19a225b722ecd6050232a38f2e6b
Dallas <dallas2k@163.com>
2020-03-20 10:57:11 +0800

    添加项目文件。

---
+++ WebApplicationTestMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationTestMVC.MvcApplication" Language="C#" %>
---
+++ WebApplicationTestMVC/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationTestMVC.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string productinfo { get; set; }
+    }
+}
