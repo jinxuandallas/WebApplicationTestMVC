436e3d267210be3fa67a1bdff2328e19d4c74d1f
Dallas <dallas2k@163.com>
2020-03-20 16:15:25 +0800

    不成功

---
+++ WebApplicationTestMVC/Concrete/EFProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplicationTestMVC.Models;
+
+namespace WebApplicationTestMVC.Concrete
+{
+    public class EFProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
---
+++ WebApplicationTestMVC/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTestMVC.Concrete;
+
+namespace WebApplicationTestMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        private EFProductRepository productRepository;
+        public TestController()
+        {
+            productRepository = new EFProductRepository();
+        }
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View(productRepository.Products);
+        }
+    }
+}
