210e3bb900921d7dd08ba5f9626a4609c98625da
Dallas <dallas2k@163.com>
2020-03-21 11:12:10 +0800

    data

--- WebApplicationTestMVC/Models/Product.cs
+++ WebApplicationTestMVC/Models/Product.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+//using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+//using System.Web.Mvc.Html;
 //using System.ComponentModel.DataAnnotations.Schema;
 namespace WebApplicationTestMVC.Models
 {
     //[Table("products")]
     public class Product
     {
+        [ScaffoldColumn(false)]
         public int id { get; set; }
         public string name { get; set; }
         public string productinfo { get; set; }
